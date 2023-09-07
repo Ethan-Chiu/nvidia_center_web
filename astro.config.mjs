47a874cc0f50c6791193092341c3151eb077d440
Ethan-Chiu <ethanchiu0608@gmail.com>
2023-09-07 23:20:30 +0800

    add shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
@@ -6,5 +6,9 @@ import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), astroI18next(), tailwind()]
+	integrations: [
+		react(),
+		astroI18next(),
+		tailwind({ applyBaseStyles: false }),
+	],
 });
