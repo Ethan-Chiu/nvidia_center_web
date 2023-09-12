4898646ee9ab712bf7a6a552ba2eebd5c9fcc2aa
Ethan-Chiu <ethanchiu0608@gmail.com>
2023-09-12 10:17:53 +0800

    add member data

--- src/schema/members.ts
+++ src/schema/members.ts
@@ -4,7 +4,9 @@ export const membersSchema = z.object({
     name: z.string(),
     title: z.string(),
     picture: z.string(),
-    name_sort: z.array(z.number())
+    name_sort: z.array(z.number()),
+    link: z.string(),
+    email: z.string(),
 })
 
 export type MembersType = z.infer<typeof membersSchema>;
