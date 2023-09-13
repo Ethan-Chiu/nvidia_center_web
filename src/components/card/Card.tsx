a0c81d4361763acb0bcaf3ecca7bb68346e7c607
Ethan-Chiu <ethanchiu0608@gmail.com>
2023-09-13 11:21:39 +0800

    update members page ui

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,11 +1,15 @@
-import "./card.css"
+import "./card.css";
 import {
 	HoverCard,
 	HoverCardContent,
 	HoverCardTrigger,
-} from "@/components/ui/hover-card"
-import { Badge } from "@/components/ui/badge"
+} from "@/components/ui/hover-card";
+import { Badge } from "@/components/ui/badge";
 import type { ReactNode } from "react";
+import { buttonVariants } from "@/components/ui/button";
+import { ExternalLink } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Separator } from "@/components/ui/separator";
 
 interface Props extends astroHTML.JSX.SelectHTMLAttributes {
 	title: string;
@@ -15,36 +19,57 @@ interface Props extends astroHTML.JSX.SelectHTMLAttributes {
 	body?: ReactNode;
 }
 
-export default function Card( { href, title, imageSrc, description, body } : Props) {
+export default function Card({
+	href,
+	title,
+	imageSrc,
+	body,
+}: Props) {
 	return (
-		<HoverCard openDelay={1000}>
-			<HoverCardTrigger asChild>
-				<li className="link-card">
-					<a href={href} className="w-full bg-secondary leading-normal rounded-md">
-						<div className="flex flex-col justify-center items-center sm:flex-row sm:items-start">
-							<img
-								className="object-cover h-40 w-32 rounded-md shadow-2xl transform -translate-y-4 border-4 border-ring"
-								src={imageSrc}
-								alt={title}
-							/>
-							<div className="grow px-4 flex-col text-foreground divide-foreground divide-y-2">
-								<p className="text-xl text-center font-bold">{title}</p>
-								<p className="px-4 py-4 text-sm font-bold text-center">
-									{description}
-								</p>
-								{body}
+		<HoverCard openDelay={700}>
+			<li className="link-card">
+				<div className="relative w-full rounded-md bg-secondary leading-normal">
+					<div className="flex flex-col items-center justify-center sm:flex-row sm:items-start">
+						<img
+							className="h-40 w-32 -translate-y-4 transform rounded-md border-4 border-ring object-cover shadow-2xl"
+							src={imageSrc}
+							alt={title}
+						/>
+						<div className="grow flex-col px-4 text-foreground">
+							<p className="text-center text-xl font-bold">
+								{title}
+							</p>
+							<Separator className="bg-foreground"/>
+							{body}
+							<div className="flex justify-end">
+								<HoverCardTrigger asChild>
+									<a
+										href={href}
+										target="_blank"
+										className={cn(
+											buttonVariants({ variant: "link" }),
+											"m-2 h-5 w-5 p-0",
+										)}
+									>
+										<ExternalLink />{" "}
+									</a>
+								</HoverCardTrigger>
 							</div>
 						</div>
-					</a>
-				</li>
-			</HoverCardTrigger>
-			<HoverCardContent className="w-full m-0 p-1" align="end">
+					</div>
+				</div>
+			</li>
+			<HoverCardContent className="m-0 w-full p-1" align="end">
 				<div className="relative p-0">
-					<Badge className="absolute -top-8 left-3" variant="destructive">Visit {title}'s website!</Badge>
+					<Badge
+						className="absolute -top-8 left-3"
+						variant="destructive"
+					>
+						Visit {title}'s website!
+					</Badge>
 					<iframe src={href}></iframe>
 				</div>
 			</HoverCardContent>
 		</HoverCard>
-	)
+	);
 }
-
