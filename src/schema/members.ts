3fff0b34471eb440f881e2e237c1a6836dc2abb1
Ethan-Chiu <ethanchiu0608@gmail.com>
2023-09-13 12:33:44 +0800

    sort member

--- src/schema/members.ts
+++ src/schema/members.ts
@@ -3,9 +3,9 @@ import { z } from 'astro:content';
 export const membersSchema = z.object({
     name: z.string(),
     title: z.string(),
-    picture: z.string(),
+    picture: z.string().url(),
     name_sort: z.array(z.number()),
-    link: z.string(),
+    link: z.string().url(),
     email: z.string(),
 })
 
