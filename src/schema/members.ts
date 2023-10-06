bdf8415663b8bb186c3ec72695aadf8208e0ff36
Ethan-Chiu <ethanchiu0608@gmail.com>
2023-10-07 00:51:45 +0800

    add member

--- src/schema/members.ts
+++ src/schema/members.ts
@@ -3,10 +3,10 @@ import { z } from 'astro:content';
 export const membersSchema = z.object({
     name: z.string(),
     title: z.string(),
-    picture: z.string().url(),
+    picture: z.string().url().nullable(),
     name_sort: z.array(z.number()),
-    link: z.string().url(),
-    email: z.string(),
+    link: z.string().url().nullable(),
+    email: z.string().email().nullable(),
 })
 
 export type MembersType = z.infer<typeof membersSchema>;
