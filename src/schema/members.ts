655dba8a70485cca6acea965bd3440c9ae76af9f
Ethan-Chiu <ethanchiu0608@gmail.com>
2023-10-07 14:24:29 +0800

    add member picture

--- src/schema/members.ts
+++ src/schema/members.ts
@@ -3,7 +3,7 @@ import { z } from 'astro:content';
 export const membersSchema = z.object({
     name: z.string(),
     title: z.string(),
-    picture: z.string().url().nullable(),
+    picture: z.string().nullable(),
     name_sort: z.array(z.number()),
     link: z.string().url().nullable(),
     email: z.string().email().nullable(),
