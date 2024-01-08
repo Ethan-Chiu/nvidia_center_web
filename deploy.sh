393842ca5883e355c8b1d7bf3b64b6af2e6246d2
Ethan-Chiu <ethanchiu0608@gmail.com>
2024-01-08 23:42:07 +0800

    remove files first

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate nvweb_node_env
 
-# Build website
-yarn install && yarn localize && yarn build
-
 # Remove all files in the directory except for 'source'
 shopt -s extglob
 rm -r -- !(source)
+
+# Build website
+yarn install && yarn localize && yarn build
 
 # Copy files from 'source/dist' to the root directory
 cp -r ./source/dist/* .
