4821011843f3d991826866f407b755fc406e6a7b
Ethan-Chiu <ethanchiu0608@gmail.com>
2024-01-08 23:22:45 +0800

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# Activate conda environment
 source ~/miniconda3/etc/profile.d/conda.sh
-
 conda activate nvweb_node_env
 
+# Build website
 yarn install && yarn localize && yarn build
 
-pm2 reload ecosystem.config.cjs --env production
+# Remove all files in the directory except for 'source'
+shopt -s extglob
+rm -r !(source)
+
+# Copy files from 'source/dist' to the root directory
+cp -r source/dist/* .
+
+echo "Cleanup and copy completed."
