a012ec6ea8348073947e3e14c6453aabea4396c7
Ethan-Chiu <ethanchiu0608@gmail.com>
2024-05-21 11:25:20 +0800

    Add astro site setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ export default defineConfig({
   server: {
     port: 3000
   },
+  site: "https://nvcenter.ntu.edu.tw",
   integrations: [react(), astroI18next(), tailwind({
     applyBaseStyles: false
   }), mdx()]
